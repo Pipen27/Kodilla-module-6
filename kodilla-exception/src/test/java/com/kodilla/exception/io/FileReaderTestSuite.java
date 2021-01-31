c0d0e3705e3691f1ff028e074ccf8e9170886a38
unknown <tomasz.ruszkowski27071989@gmail.com>
2021-01-31 17:46:06 +0100

    8.4 prepare

---
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/FileReaderTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.exception.io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FileReaderTestSuite {
+
+    @Test
+    void testReadFile() {
+        // given
+        FileReader fileReader = new FileReader();
+        // when & then
+        assertDoesNotThrow(() -> fileReader.readFile());
+    }
+
+    @Test
+    void whenFileDosentExistsReadFileShouldThrowException() {
+        // given
+        FileReader fileReader = new FileReader();
+        String fileName = "nie-ma.txt";
+        // when & then
+        assertThrows(FileReaderException.class, () -> fileReader.readFile1(fileName));
+    }
+}

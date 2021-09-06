53963986abe3f1883929892e557456959c7771b9
unknown <tomasz.ruszkowski27071989@gmail.com>
2021-09-06 20:38:30 +0200

    Fix for flight search 13.5

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flightSerach/SearchFlight.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flightSerach/SearchFlight.java
@@ -1,35 +1,52 @@
 package com.kodilla.good.patterns.challenges.flightSerach;
 
+import java.util.stream.Collectors;
+
 public class SearchFlight {
 
-    public void searchFlightFrom(User user, FlightList flightList)
+    public FlightList searchFlightFrom(User user, FlightList flightList)
     {
         flightList.getFlightList().entrySet().stream()
                 .map(e -> e.getValue())
                 .filter(e -> e.getDepartureAirport().equals(user.getAirport().getDepartureAirport().toUpperCase()))
                 .map(e -> "Departure from: " + e.getDepartureAirport() + " ---> " + "Arrival to: " + e.getArrivalAirport())
+                .collect(Collectors.toList())
                 .forEach(System.out::println);
         System.out.println();
+            return flightList;
+
     }
 
-    public void searchFlightTo(User user, FlightList flightList)
+    public FlightList searchFlightTo(User user, FlightList flightList)
     {
         flightList.getFlightList().entrySet().stream()
                 .map(e -> e.getValue())
                 .filter(e -> e.getArrivalAirport().equals(user.getAirport().getArrivalAirport().toUpperCase()))
                 .map(e -> "Arrival to: " + e.getArrivalAirport() + " <--- " + "Departure from: " + e.getDepartureAirport())
+                .collect(Collectors.toList())
                 .forEach(System.out::println);
         System.out.println();
+            return flightList;
     }
 
-    public void searchOneStopFlight(User user, FlightList flightList)
+    public FlightList searchOneStopFlight(User user, FlightList flightList)
     {
         flightList.getFlightList().entrySet().stream()
                 .map(e -> e.getValue())
-                .filter(e -> e.getDepartureAirport().equals(user.getAirport().getDepartureAirport().toUpperCase()) || e.getArrivalAirport()
-                        .equals(user.getAirport().getArrivalAirport().toUpperCase()))
-                .filter(e -> e.getDepartureAirport().equals(user.getOneStopConnection().toUpperCase()) || e.getArrivalAirport().equals(user.getOneStopConnection().toUpperCase()))
-                .map(e -> "Departure from: " + e.getDepartureAirport() + " ---> " + "Arrival to: " + e.getArrivalAirport())
+                .filter(e -> e.getDepartureAirport().equals(user.getAirport().getDepartureAirport().toUpperCase()) && e.getArrivalAirport()
+                        .equals(user.getOneStopConnection().toUpperCase()))
+                .map(e -> "Departure from: " + e.getDepartureAirport() + " ---> " + "Arrival to (One stop Airport): " + e.getArrivalAirport())
+                .collect(Collectors.toList())
                 .forEach(System.out::println);
+
+        flightList.getFlightList().entrySet().stream()
+                .map(e -> e.getValue())
+                .filter(e -> e.getDepartureAirport().equals(user.getOneStopConnection().toUpperCase()) && e.getArrivalAirport().equals(user.getAirport().getArrivalAirport().toUpperCase()))
+                .map(e -> "Departure from (One stop Airport): " + e.getDepartureAirport() + " ---> " + "Arrival to: " + e.getArrivalAirport())
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+        System.out.println();
+            return flightList;
     }
 }
